2269998e768570fcf1ca0635c9c0fca16cfe3b06
dilshad51 <dilshadara51@gmail.com>
2026-06-04 02:36:08 +0530

    Added Add to Array-Form of Integer

---
+++ 0989_Add_to_Array_Form_of_Integer.java
@@ -0,0 +1,38 @@
+/*
+989. Add to Array-Form of Integer
+
+Link:
+https://leetcode.com/problems/add-to-array-form-of-integer/
+*/
+
+import java.util.*;
+
+class Solution {
+
+    // Main method
+    public static void main(String[] args) {
+
+        int[] num = {1, 2, 0, 0};
+        int k = 34;
+
+        List<Integer> result = addToArrayForm(num, k);
+
+        System.out.println(result);
+    }
+
+    // Solution method
+    static List<Integer> addToArrayForm(int[] num, int k) {
+        ArrayList<Integer> ans=new ArrayList<>();
+        int i=nums.length-1;
+        while(i>=0||k>0){
+            if(i>=0){
+                k+=nums[i];
+                i--;
+            }
+            ans.add(k%10);
+            k/=10;
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
