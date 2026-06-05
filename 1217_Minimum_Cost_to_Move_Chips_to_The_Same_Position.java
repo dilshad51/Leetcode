4b06ff80601338f0698733480247e770b97a4918
dilshad51 <dilshadara51@gmail.com>
2026-06-05 23:29:28 +0530

    Added Minimum Cost to Move Chips to The Same Position

--- 1217_Minimum_Cost_to_Move_Chips_to_The_Same_Position.java
+++ 1217_Minimum_Cost_to_Move_Chips_to_The_Same_Position.java
@@ -1,3 +1,32 @@
-public class 1217_Minimum_Cost_to_Move_Chips_to_The_Same_Position {
-    
+/*
+1217. Minimum Cost to Move Chips to The Same Position
+
+Link:
+https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/
+*/
+
+import java.util.*;
+
+class Solution {
+
+    // Main method
+    public static void main(String[] args) {
+
+        int[] position = {1, 2, 3};
+
+        int result = minCostToMoveChips(position);
+
+        System.out.println(result);
+    }
+
+    // Solution method
+    static int minCostToMoveChips(int[] position) {
+        int odd=0;
+        int even =0;
+        for(int i=0;i<position.length;i++){
+            if(position[i]%2==0) even++;
+            else odd++;
+        }
+        return Math.min(odd,even);
+    }
 }
